8d896cfe3babd0002290145dc6a44b9d97bd37ef
Rahul <rahulbiswalnew@gmail.com>
2023-06-03 15:03:00 +0530

    second routes and signup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { SellerAuthComponent } from './seller-auth/seller-auth.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component: HomeComponent,
+  },
+  {
+    path:'seller-auth',
+    component: SellerAuthComponent,
+  },
+  {
+    path:'',
+    component: HomeComponent,
+  },
+  {
+    path:'',
+    component: HomeComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
